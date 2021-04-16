6cb805a08c1517e0fec160368e56883d590be8ff
Jens <jenscollaertprive@hotmail.com>
2021-04-16 15:26:00 +0200

    added working msql banning and unbanning

--- src/main/java/com/alysaa/geyseradmintools/forms/BanPlayerForm.java
+++ src/main/java/com/alysaa/geyseradmintools/forms/BanPlayerForm.java
@@ -2,9 +2,9 @@ package com.alysaa.geyseradmintools.forms;
 
 import com.alysaa.geyseradmintools.Gat;
 import com.alysaa.geyseradmintools.database.MySql;
-import com.alysaa.geyseradmintools.listeners.AdminToolOnLogin;
 import com.alysaa.geyseradmintools.utils.CheckJavaOrFloodPlayer;
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.geysermc.cumulus.CustomForm;
 import org.geysermc.cumulus.SimpleForm;
@@ -18,7 +18,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 public class BanPlayerForm {
@@ -60,6 +59,7 @@ public class BanPlayerForm {
         UUID uuid = player.getUniqueId();
         List<String> names = Bukkit.getOnlinePlayers().stream().map(Player::getName).collect(Collectors.toList());
         String[] playerlist = names.toArray(new String[0]);
+
         boolean isFloodgatePlayer = CheckJavaOrFloodPlayer.isFloodgatePlayer(uuid);
         if (isFloodgatePlayer) {
             FloodgatePlayer fplayer = FloodgateApi.getInstance().getPlayer(uuid);
@@ -102,27 +102,23 @@ public class BanPlayerForm {
     }
 
     public static void unbanPlayers(Player player) {
-        List<String> names = new ArrayList<String>();
         UUID uuid = player.getUniqueId();
-        try {
-            PreparedStatement statement = MySql.getConnection()
-                    .prepareStatement("SELECT * FROM " + MySql.Bantable + " WHERE UUID=?");
-            statement.setString(1, uuid.toString());
-            ResultSet results = statement.executeQuery();
-            while (results.next()) {
-                String uname = results.getString("Username");
-                names.add(uname);
+        List<String> names = new ArrayList<>();
+        String query = "SELECT * FROM " + MySql.Bantable;
+        try (Statement stmt = MySql.getConnection().createStatement()) {
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                names.add(rs.getString("Username"));
             }
-        } catch (SQLException exe) {
-            exe.printStackTrace();
-        }
-        boolean isFloodgatePlayer = CheckJavaOrFloodPlayer.isFloodgatePlayer(uuid);
+            rs.close();
+            String[] playerlist = names.toArray(new String[0]);
+            boolean isFloodgatePlayer = CheckJavaOrFloodPlayer.isFloodgatePlayer(uuid);
             if (isFloodgatePlayer) {
                 FloodgatePlayer fplayer = FloodgateApi.getInstance().getPlayer(uuid);
                 fplayer.sendForm(
                         CustomForm.builder()
                                 .title("unban tool")
-                                .dropdown("Select Player to unban", names.toString())
+                                .dropdown("Select Player to unban", playerlist)
                                 .responseHandler((form, responseData) -> {
                                     CustomFormResponse response = form.parseResponse(responseData);
                                     if (!response.isCorrect()) {
@@ -130,25 +126,23 @@ public class BanPlayerForm {
                                     }
                                     int clickedIndex = response.getDropdown(0);
                                     String name = names.get(clickedIndex);
-                                    Player player1 = Bukkit.getPlayer(name);
-                                    System.out.println(player1);
+                                    OfflinePlayer player1 = Bukkit.getOfflinePlayer(name);
                                     //MySQL code
                                     if (Gat.plugin.getConfig().getBoolean("EnableMySQL")) {
                                         try {
-                                            PreparedStatement statements = MySql.getConnection()
-                                                    .prepareStatement("SELECT * FROM " + MySql.Bantable + " WHERE UUID=?");
-                                            statements.setString(1, uuid.toString());
+                                            PreparedStatement statement = MySql.getConnection()
+                                                    .prepareStatement("DELETE FROM " + MySql.Bantable + " WHERE UUID=?");
+                                            statement.setString(1, player1.getUniqueId().toString());
+                                            statement.execute();
 
-                                            ResultSet results = statements.executeQuery();
-                                            while (results.next()) {
-                                                statements.setString(1, player1.getUniqueId().toString());
-                                                statements.execute();
-                                            }
-                                        } catch (SQLException throwables) {
-                                            throwables.printStackTrace();
+                                        } catch (SQLException exe) {
+                                            exe.printStackTrace();
                                         }
                                     }
                                 }));
             }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
