c4c9bce981587fc8ba7dc3bf5659da8af7cf0498
Jens <jenscollaertprive@hotmail.com>
2021-04-17 21:23:09 +0200

    Added mute and ban check on player login to see if ban time has been passed.

--- src/main/java/com/alysaa/geyseradmintools/listeners/AdminToolOnLogin.java
+++ src/main/java/com/alysaa/geyseradmintools/listeners/AdminToolOnLogin.java
@@ -1,6 +1,7 @@
 package com.alysaa.geyseradmintools.listeners;
 
 import com.alysaa.geyseradmintools.database.BanDatabaseSetup;
+import com.alysaa.geyseradmintools.database.MuteDatabaseSetup;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -10,9 +11,77 @@ import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.UUID;
 
 public class AdminToolOnLogin implements Listener {
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void checkMute(AsyncPlayerPreLoginEvent e) {
+        UUID uuid = e.getUniqueId();
+        try {
+            PreparedStatement statement = MuteDatabaseSetup.getConnection()
+                    .prepareStatement("SELECT * FROM " + MuteDatabaseSetup.Mutetable + " WHERE UUID=?");
+            statement.setString(1, uuid.toString());
+            ResultSet results = statement.executeQuery();
+            while (results.next()) {
+                String enddate = results.getString("EndDate");
+                Date datadate = new SimpleDateFormat("yyyy-MM-dd").parse(enddate);
+                String curdate = LocalDate.now().toString();
+                Date currdate = new SimpleDateFormat("yyyy-MM-dd").parse(curdate);
+                if (datadate.compareTo(currdate) > 0) {
+                   return;
+                } else if (datadate.compareTo(currdate) < 0) {
+                    try {
+                        statement = MuteDatabaseSetup.getConnection()
+                                .prepareStatement("DELETE FROM " + MuteDatabaseSetup.Mutetable + " WHERE UUID=?");
+                        statement.setString(1, e.getUniqueId().toString());
+                        statement.execute();
+
+                    } catch (SQLException exe) {
+                        exe.printStackTrace();
+                    }
+                }
+
+            }
+        } catch (SQLException | ParseException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+    public void checkBan(AsyncPlayerPreLoginEvent e) {
+        UUID uuid = e.getUniqueId();
+        try {
+            PreparedStatement statement = BanDatabaseSetup.getConnection()
+                    .prepareStatement("SELECT * FROM " + BanDatabaseSetup.Bantable + " WHERE UUID=?");
+            statement.setString(1, uuid.toString());
+            ResultSet results = statement.executeQuery();
+            while (results.next()) {
+                String enddate = results.getString("EndDate");
+                Date datadate = new SimpleDateFormat("yyyy-MM-dd").parse(enddate);
+                String curdate = LocalDate.now().toString();
+                Date currdate = new SimpleDateFormat("yyyy-MM-dd").parse(curdate);
+                if (datadate.compareTo(currdate) > 0) {
+                    return;
+                } else if (datadate.compareTo(currdate) < 0) {
+                    try {
+                        statement = MuteDatabaseSetup.getConnection()
+                                .prepareStatement("DELETE FROM " + BanDatabaseSetup.Bantable + " WHERE UUID=?");
+                        statement.setString(1, e.getUniqueId().toString());
+                        statement.execute();
+
+                    } catch (SQLException exe) {
+                        exe.printStackTrace();
+                    }
+                }
+
+            }
+        } catch (SQLException | ParseException throwables) {
+            throwables.printStackTrace();
+        }
+    }
     @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
     public void onPlayerLogin(AsyncPlayerPreLoginEvent e) {
         UUID uuid = e.getUniqueId();
