1efe57bba6d1ce5f4569e5f278d9808042346ac1
Jens <jenscollaertprive@hotmail.com>
2021-04-19 10:28:15 +0200

    added mute command not yet working

--- src/main/java/com/alysaa/geyseradmintools/commands/UnbanCommand.java
+++ src/main/java/com/alysaa/geyseradmintools/commands/UnbanCommand.java
@@ -4,49 +4,55 @@ import com.alysaa.geyseradmintools.Gat;
 import com.alysaa.geyseradmintools.database.BanDatabaseSetup;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
-public class BanCommand implements CommandExecutor {
+public class UnbanCommand {
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
         if (!(sender instanceof Player)) {
             sender.sendMessage(ChatColor.RED + "The console cannot use this command");
             return true;
         }
+        List<String> names = new ArrayList<>();
+        String query = "SELECT * FROM " + BanDatabaseSetup.Bantable;
+        try (Statement stmt = BanDatabaseSetup.getConnection().createStatement()) {
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                names.add(rs.getString("Username"));
+            }
+            rs.close();
         Player player = (Player) sender;
         if (cmd.getName().equalsIgnoreCase("gban") && player.hasPermission("geyseradmintools.gban")) {
-            Player target = Bukkit.getServer().getPlayer(args[0]);
-            String day = args[1];
-            String time = LocalDate.now().plusDays(Long.parseLong(day)).toString();
-            String reason = args[2];
+            OfflinePlayer target = Bukkit.getOfflinePlayer();
             if (target == null) {
                 player.sendMessage(ChatColor.RED + "[GeyserAdminTools] Could not find player!");
                 player.sendMessage(ChatColor.RED + "[GeyserAdminTools] Perhaps wrong usage ? /gban <username> <amount of days> <reason>");
                 return true;
             }
             try {
-                String sql = "(UUID,REASON,USERNAME,ENDDATE) VALUES (?,?,?,?)";
-                PreparedStatement insert = BanDatabaseSetup.getConnection().prepareStatement("INSERT INTO " + BanDatabaseSetup.Bantable
-                        + sql);
-                insert.setString(1, target.getUniqueId().toString());
-                insert.setString(2, reason);
-                insert.setString(3, target.getName());
-                insert.setString(4, time);
-                insert.executeUpdate();
-                // Player inserted now
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
+                PreparedStatement statement = BanDatabaseSetup.getConnection()
+                        .prepareStatement("DELETE FROM " + BanDatabaseSetup.Bantable + " WHERE UUID=?");
+                statement.setString(1, target.getUniqueId().toString());
+                statement.execute();
+
+            } catch (SQLException exe) {
+                exe.printStackTrace();
             }
-            target.kickPlayer("you where banned for: " + reason);
             Gat.logger.info("Player " + player.getName() + " has banned " + target.getName() + " till: " + time + " for reason: " + reason);
             //end
         }
         return true;
+    } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
-}
